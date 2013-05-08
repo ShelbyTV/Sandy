f65356ebcaacf0826a2ff9d4f47a679c04df595b
Michael Matyus <michael@maty.us>
2013-05-08 12:05:51 -0400

    flow done, tweaking markup

--- js/sandy-generic-interactions.js
+++ js/sandy-generic-interactions.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 
   //buttons that toggle themselves, and nothing more
-  $('.js-button-toggle').on('click',function(){
+  $('.js-button-toggle').on('click',function(e){
     $(this).toggleClass('button_active',!$(this).hasClass('button_active'));
+  });
+
+  $('.js-social-toggle').on('click',function(e){
+    e.preventDefault();
+    $(this).toggleClass('button_gray disabled',!$(this).hasClass('button_gray disabled'));
   });
 });
