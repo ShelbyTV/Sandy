f43c67b744b20652e2cc34843f2b3f9e0a144793
Michael Matyus <michael@maty.us>
2013-05-09 11:17:21 -0400

    moving some code around, created benign sample for future use

--- js/sandy-generic-interactions.js
+++ js/sandy-generic-interactions.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 
+  State = {
+    //cache shelves
+    $documentShelves : $('.shelf'),
+
+    getParams : function() {
+      return window.location.hash.split('#')[1];
+    },
+
+    DocumentState : function(index) {
+      console.log('documentstate',index);
+      index = index || this.getParams() || 'init';
+      console.log('documentstate param',index);
+
+      this.$documentShelves.toggleClass('hidden', true);
+      $('.shelf--'+index).toggleClass('hidden',false);
+
+      this.updateState(index);
+    },
+
+    updateState : function(state) {
+      history.pushState(null,null,'#'+state);
+    }
+  };
+
+
   //buttons that toggle themselves, and nothing more
   $('.js-button-toggle').on('click',function(e){
     $(this).toggleClass('button_active',!$(this).hasClass('button_active'));
@@ -9,4 +34,26 @@ $(document).ready(function(){
     e.preventDefault();
     $(this).toggleClass('button_gray disabled',!$(this).hasClass('button_gray disabled'));
   });
+
+    //basic functioning of a progressive flow.
+    $('.js-next').on('submit',function(e){
+      e.preventDefault();
+
+      var nextSlide = $(this).data('go_to');
+
+      State.DocumentState(nextSlide);
+    });
+
+    $('.js-split-decision').on('click',function(e){
+      e.preventDefault();
+
+      var nextSlide = $(this).data('go_to');
+
+      State.DocumentState(nextSlide);
+    });
+
+    //handle for backbutton
+    $(window).hashchange(function(){
+      State.DocumentState();
+    });
 });
