3054a4247ebe2a62bc6e8e94c9c103b344aae751
Michael Matyus <michael@maty.us>
2013-07-15 15:15:41 -0400

    The new sign up flow is now fleshed out based on the recent wireframes.
    Obviously at this point "design" leaves something to be desired.

---
+++ signup-13-07-15/js/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+  {
+    nickname : 'Henry',
+    personalRollId : '4f907a9b9a725b46010005c7',
+    description : 'henry',
+    avatar : 'images/avatar.jpg'
+  },
+  {
+    nickname : 'Vincent Iadevaia',
+    personalRollId : '4f9577c488ba6b15ec0084c7',
+    description : 'vondoom',
+    avatar : 'images/avatar.jpg'
+  },
+  {
+    nickname : 'Christopher Kurdziel',
+    personalRollId : '4fbbc4211c1cf42b91002eac',
+    description : 'ckurdziel',
+    avatar : 'images/avatar.jpg'
+  }
+];
